24dc03b251635f24080483fad9f14a2eca873bd2
PRINCEWILL AMBE <nebaprincewill237@gmail.com>
2023-05-31 10:15:45 +0100

    Merge branch 'main' of https://github.com/WillAmb-Cop/Good-App

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,5 +1,10 @@
 import React from 'react'
 import './contact.css'
+import {MdOutlineEmail} from 'react-icons/md'
+import {AiOutlineLinkedin} from 'react-icons/ai'
+import {BsWhatsapp} from 'react-icons/bs'
+import {useRef} from 'react'
+
 const Contact = () => {
   return (
     <section id='contact'>Contact</section>
